12a8ba2e9111dad8fed02fea9d98a35704311de0
jiylee <jjenny4711@gmail.com>
2024-06-15 02:10:31 -0400

    test

--- utils/captions.js
+++ utils/captions.js
@@ -2,19 +2,38 @@
 require('dotenv').config();
 const axios = require('axios');
 
+
+const findLanguage=async(videoId)=>{
+  try{
+    const url = "https://www.searchapi.io/api/v1/search";
+const params = {
+  "engine": "youtube_video",
+  "video_id": videoId,
+  "api_key": "HAZH2Utd9CW7RgFgdVQ8CUtV"
+};
+
+    const res = await axios.get(url, { params });
+console.log(res.data.available_transcripts_languages,'res.data')
+return res.data.available_transcripts_languages;
+  }catch(error){
+    console.log(error,'error-findLanguage')
+  }
+}
+
  const searchApiCaption =async(videoId)=>{
 
   try{
+    const lang=await findLanguage(videoId);
     const url = "https://www.searchapi.io/api/v1/search";
     const params = {
       "engine": "youtube_transcripts",
       "video_id": videoId,
-      
+   
       "api_key": process.env.SEARCHAPI_API_KEY,
     };
 
 const res = await axios.get(url, { params });
-console.log(res.data.transcripts,'res.data')
+console.log(lang,'res.data-lang')
 return res.data.transcripts;
 
   }catch(error){
